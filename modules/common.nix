fb96229b1a7ef1fbec0f9cfa380a1ee023e0d95e
Colm Murphy <colmmurphy016@gmail.com>
2025-10-11 16:07:11 +0100

    update common.nix

--- modules/common.nix
+++ modules/common.nix
@@ -1,3 +1,33 @@
-{ ... }: {
+{ config, pkgs, libs, ... }:
+{
+  time.timeZone = "Europe/Dublin";
+  i18n.defaultLocale = "en_IE.UTF-8";
 
+  environment.systemPackages = with pkgs; [
+    curl
+    dig
+    direnv
+    fastfetch
+    file
+    git
+    htop
+    neovim
+    nixfmt-rfc-style
+    nix-direnv
+    tree
+    unzip
+    wget
+    vim
+    wget
+  ];
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    auto-optimise-store = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
 }
