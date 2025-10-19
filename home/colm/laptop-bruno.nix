22faa7dfc8dacd987e28842b9bcc2f18987b185d
Colm Murphy <colm@colmmurphy.xyz>
2025-10-19 14:44:40 +0100

    create home-manager file for laptop

---
+++ home/colm/laptop-bruno.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./features/bash
+    ./features/git.nix
+  ];
+
+  programs.home-manager.enable = true;
+  home.username = "colm";
+  home.homeDirectory = "/home/colm";
+
+  home.stateVersion = "24.11";
+
+  home.packages = [
+
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+}
