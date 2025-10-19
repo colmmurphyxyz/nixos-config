dbf57d2fcc7aa3ece433419b1d3e61af3a691d85
Colm Murphy <colm@colmmurphy.xyz>
2025-10-19 17:23:27 +0100

    set keybinds using dconf.settings

--- home/colm/features/keybinds/default.nix
+++ home/colm/features/keybinds/default.nix
@@ -5,29 +5,34 @@
     xdotool
   ];
 
-  xdg.gnome.keybindings = {
-    "close-window" = {
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/close-window/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/maximize-window/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot-area/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/close-window" = {
       name = "Close Window";
       command = "xdotool getwindowfocus windowclose";
       binding = "<Super>q";
     };
-
-    "maximize-window" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/maximize-window" = {
       name = "Maximize Window";
       command = "xdotool getwindowfocus windowsize 100% 100%";
       binding = "<Super>m";
     };
-
-    "screenshot-area" = {
-      name = "Take Area Screenshot";
-      command = "gnome-screenshot -a";
-      binding = "<Shift><Print>";
-    };
-
-    "open-terminal" = {
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal" = {
       name = "Open Terminal";
       command = "kgx";
-      binding = "<Super>t";
+      binding = "<Ctrl><Alt>t";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot-area" = {
+      name = "Take Area Screenshot";
+      command = "gnome-screenshot -a";
+      binding = "<Super><Shift>S";
     };
   };
 }
