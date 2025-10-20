d46781f69185661561cb6e692f1c6a7bec2584f7
Colm Murphy <colmmurphy016@gmail.com>
2025-10-20 20:33:26 +0100

    fix typo

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
             ./hosts/pc/hardware-configuration.nix
             ./modules/common.nix
             ./modules/git.nix
-            ./modules/gnupg.git
+            ./modules/gnupg.nix
             ./modules/htop.nix
             ./modules/nvidia.nix
             ./modules/steam.nix
