90e7213fdc4269dc70abc77a16f461e467e70063
Colm Murphy <colm@colmmurphy.xyz>
2025-10-28 18:53:54 +0000

    comment

--- modules/gnupg.nix
+++ modules/gnupg.nix
@@ -7,6 +7,8 @@
 
   programs.gnupg.agent = {
     enable = true;
+    # note to self: gpg can't find pinentry on my lapptop without changing ~/.gnupg/gpg-agent.cong
+    # consider doing this via home-manager or whatevs
     pinentryPackage = pkgs.pinentry-all;
   };
 }
