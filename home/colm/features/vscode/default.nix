743b1d2920814de4f45c99424f51e88588cd27f9
Colm Murphy <colmmurphy016@gmail.com>
2025-11-16 14:34:57 +0000

    Add darcula vscode theme

--- home/colm/features/vscode/default.nix
+++ home/colm/features/vscode/default.nix
@@ -1,17 +1,28 @@
 { pkgs, ... }:
+let
+  other-extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "vscode-theme-darcula";
+      publisher = "rokoroku";
+      version = "1.2.3";
+      sha256 = "tlI+3DyDPo9tTOVTrEHoOtVTnEYSjgu9wojTXmEvv4E=";
+    }
+  ];
+in
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       astro-build.astro-vscode
       eamodio.gitlens
       haskell.haskell
       jnoortheen.nix-ide
       justusadam.language-haskell
       vscodevim.vim
-    ];
+    ] ++ other-extensions;
 
-    profiles.colm.userSettings = {
+    profiles.default.userSettings = {
+      "workbench.colorTheme" = "Darcula";
       "[nix]"."editor.tabSize" = 2;
     };
   };
