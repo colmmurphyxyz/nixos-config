a99c7be18334ada00a20fad0c620420caaf1f4e3
Colm Murphy <colmmurphy016@gmail.com>
2026-01-25 15:54:35 +0000

    use jetbrains.idea package for intellij

--- home/colm/pc-leo.nix
+++ home/colm/pc-leo.nix
@@ -21,7 +21,7 @@
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
-    jetbrains.idea-community
+    jetbrains.idea
   ];
 
   programs.bash = {
